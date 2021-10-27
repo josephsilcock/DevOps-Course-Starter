d9d03d096d656b736548ec9eb6c291ac10b87556
Joseph Silcock <joseph.silcock@softwire.com>
2021-10-27 16:15:23 +0100

    Update index to list of todo items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 
+from todo_app.data.session_items import get_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,4 +9,4 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template("index.html", items=get_items())
