32ff57e0121af17681412b3c3c8edc209023071e
Joseph Silcock <joseph.silcock@softwire.com>
2022-02-02 10:53:22 +0000

    Move in progress and not started to property functions

--- todo_app/data/items.py
+++ todo_app/data/items.py
@@ -23,8 +23,6 @@ class Item:
 class ItemView:
     def __init__(self, items: List[Item]):
         self.sorted_by_due_items = sorted(items, key=lambda item: (item.due is None, item.due))
-        self.in_progress = [item for item in self.sorted_by_due_items if item.status == Status.IN_PROGRESS]
-        self.not_started = [item for item in self.sorted_by_due_items if item.status == Status.NOT_STARTED]
         # TODO hook this up to frontend
         self.should_show_all_done_items = False
 
@@ -38,3 +36,11 @@ class ItemView:
         if self.should_show_all_done_items or len(ordered_by_completion) < 5:
             return ordered_by_completion
         return [item for item in ordered_by_completion if item.last_modification.date() == datetime.today().date()]
+
+    @property
+    def in_progress(self) -> List[Item]:
+        return [item for item in self.sorted_by_due_items if item.status == Status.IN_PROGRESS]
+
+    @property
+    def not_started(self) -> List[Item]:
+        return [item for item in self.sorted_by_due_items if item.status == Status.NOT_STARTED]
