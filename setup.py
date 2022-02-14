72794fade685668bcd4a27575d9d4672c0e3ce7f
Joseph Silcock <joseph.silcock@softwire.com>
2022-02-14 18:54:34 +0000

    Remove file

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ import os
 import shutil
 
 if shutil.which("poetry") is None:
-    os.system("wget -o install_poetry.py https://install.python-poetry.org")
+    os.system("curl -o install_poetry.py https://install.python-poetry.org")
     f = open("install_poetry.py").read()
+    os.remove("install_poetry.py")
     code = compile(f, "install_poetry.py", 'exec')
     exec(code)
 
