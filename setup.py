6ce4e5da1b38c998bddcd75c9b6e578d1056a76e
Joseph Silcock <joseph.silcock@softwire.com>
2022-02-14 19:11:03 +0000

    Custom .env

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 import os
 import shutil
 
+# Install Poetry
 if shutil.which("poetry") is None:
     os.system("curl -o install_poetry.py https://install.python-poetry.org")
-    f = open("install_poetry.py").read().replace("sys.exit(main())", "main()")
+    install_poetry_file = open("install_poetry.py").read().replace("sys.exit(main())", "main()")
     os.remove("install_poetry.py")
-    code = compile(f, "install_poetry.py", 'exec')
+    code = compile(install_poetry_file, "install_poetry.py", 'exec')
     exec(code)
 
+# Install Dependencies
 os.system("poetry install")
-shutil.copy(".env.template", ".env")
+
+# Create .env
+if not os.path.exists(".env"):
+    env_file = open(".env.template").read()
+    env_file = env_file.replace("secret-key", input("What is the secret key? "))
+    env_file = env_file.replace("trello-key", input("What is the trello key? "))
+    env_file = env_file.replace("trello-token", input("What is the trello token? "))
+    env_file = env_file.replace("trello-board-id", input("What is the trello board id? "))
+
+    with open(".env", "w+") as f:
+        print(env_file, file=f)
+
+# Start App
 os.system("poetry run flask run")
