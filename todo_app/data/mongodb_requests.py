4b2f9a6c77f37748764c476133bb215c6628ebff
Joseph Silcock <joseph.silcock@softwire.com>
2022-08-03 10:18:43 +0100

    Use MongoDb

---
+++ todo_app/data/mongodb_requests.py
@@ -0,0 +1,44 @@
+import os
+from datetime import datetime
+from typing import Dict, Union
+
+from pymongo import MongoClient
+
+from todo_app.data.items import Item, ItemView, Status
+
+
+class MongoDbRequests:
+    def __init__(self):
+        client = MongoClient(os.getenv("MONGODB_CONNECTION_STRING"))
+        db = client[os.getenv("DB_NAME")]
+        self.collection = db["items"]
+
+    def _json_to_item(self, json: Dict[str, Union[str, datetime]]) -> Item:
+        return Item(
+            id_=json["_id"],
+            status=Status(json["status"]),
+            title=json["title"],
+            description=json["description"],
+            last_modification=json["last_modified"],
+            due=datetime.fromisoformat(json["due"]) if json["due"] else None,
+        )
+
+    def get_items(self) -> ItemView:
+        return ItemView([self._json_to_item(item) for item in self.collection.find()])
+
+    def add_item(self, title: str, description: str, due: str) -> None:
+        self.collection.insert_one(
+            {
+                "title": title,
+                "status": Status.NOT_STARTED.value,
+                "description": description,
+                "due": due,
+                "last_modified": datetime.now(),
+            }
+        )
+
+    def remove_item(self, id_: str) -> None:
+        self.collection.delete_one({"_id": id_})
+
+    def update_item_status(self, id_: str, new_status: Status) -> None:
+        self.collection.update_one({"_id": id_}, {"$set": {"status": new_status.value, "lastModified": datetime.now()}})
