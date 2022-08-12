8bc4a9138df9ad92814493c47034cc22f155cfa8
Joseph Silcock <joseph.silcock@softwire.com>
2022-08-12 12:30:29 +0100

    Fix typo

--- todo_app/app.py
+++ todo_app/app.py
@@ -9,32 +9,32 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config())
 
-    mongodb_reqeusts = MongoDbRequests()
+    mongodb_requests = MongoDbRequests()
 
     @app.route("/")
     def index():
-        return render_template("index.html", items=mongodb_reqeusts.get_items())
+        return render_template("index.html", items=mongodb_requests.get_items())
 
     @app.route("/add-item", methods=["POST"])
     def add_item_to_items():
-        mongodb_reqeusts.add_item(
+        mongodb_requests.add_item(
             request.values.get("title"), request.values.get("description"), request.values.get("due-date")
         )
         return redirect("/")
 
     @app.route("/delete-item", methods=["POST"])
     def delete_item():
-        mongodb_reqeusts.remove_item(request.values.get("id"))
+        mongodb_requests.remove_item(request.values.get("id"))
         return redirect("/")
 
     @app.route("/complete-item", methods=["POST"])
     def complete_item():
-        mongodb_reqeusts.update_item_status(request.values.get("id"), Status.COMPLETED)
+        mongodb_requests.update_item_status(request.values.get("id"), Status.COMPLETED)
         return redirect("/")
 
     @app.route("/start-item", methods=["POST"])
     def start_item():
-        mongodb_reqeusts.update_item_status(request.values.get("id"), Status.IN_PROGRESS)
+        mongodb_requests.update_item_status(request.values.get("id"), Status.IN_PROGRESS)
         return redirect("/")
 
     return app
